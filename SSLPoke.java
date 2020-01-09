167c7558cb052b8e408b9402adc143efb3c72ff9
Michael Gebetsroither <michael@mgeb.org>
2020-01-09 21:17:55 +0100

    implement proxy support

--- SSLPoke.java
+++ SSLPoke.java
@@ -1,3 +1,4 @@
+import java.net.Socket;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
@@ -10,16 +11,36 @@ public class SSLPoke {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: "+SSLPoke.class.getName()+" <host> <port>");
+            System.out.println("   use proxy with: -Dhttps.proxyHost=<proxy_host> -Dhttps.proxyPort=<proxy_port>");
             System.exit(1);
         }
         try {
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
+            SSLSocket sslsocket;
+
             SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
+
+            String tunnel_host = System.getProperty("https.proxyHost");
+            Integer tunnel_port_i = Integer.getInteger("https.proxyPort");
+            int tunnel_port = 0;
+
+            if (tunnel_host != null && tunnel_port_i != null) {
+                // tls through tunnel
+                tunnel_port = tunnel_port_i.intValue();
+                System.err.println("Using proxy: " + tunnel_host + ":" + tunnel_port);
+                Socket tunnel = new Socket(tunnel_host, tunnel_port);
+                open_http_connect_tunnel(tunnel, tunnel_host, tunnel_port, host, port);
+                sslsocket = (SSLSocket) sslsocketfactory.createSocket(tunnel, host, port, true);
+            } else {
+                // tls direct connect
+                sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
+            }
 
             InputStream in = sslsocket.getInputStream();
             OutputStream out = sslsocket.getOutputStream();
 
-            // Write a test byte to get a reaction :)
+            // Write a test byte to get a reaction
             out.write(1);
 
             while (in.available() > 0) {
@@ -30,5 +51,60 @@ public class SSLPoke {
         } catch (Exception exception) {
             exception.printStackTrace();
         }
+    }
+
+    private static void open_http_connect_tunnel(Socket tunnel, String tunnel_host, int tunnel_port, String host, int port)
+    throws IOException
+    {
+        OutputStream out = tunnel.getOutputStream();
+        String msg = "CONNECT " + host + ":" + port + " HTTP/1.1\n"
+                     + "User-Agent: " + sun.net.www.protocol.http.HttpURLConnection.userAgent + "\r\n"
+                     + "\r\n";
+        byte b[];
+        try {
+            b = msg.getBytes("ASCII7");
+        } catch (UnsupportedEncodingException ignored) {
+            b = msg.getBytes();
+        }
+        out.write(b);
+        out.flush();
+
+        byte            reply[] = new byte[200];
+        int             replyLen = 0;
+        int             newlinesSeen = 0;
+        boolean         headerDone = false;     /* Done on first newline */
+
+        InputStream     in = tunnel.getInputStream();
+        boolean         error = false;
+
+        while (newlinesSeen < 2) {
+            int i = in.read();
+            if (i < 0) {
+                throw new IOException("Unexpected EOF from proxy");
+            }
+            if (i == '\n') {
+                headerDone = true;
+                ++newlinesSeen;
+            } else if (i != '\r') {
+                newlinesSeen = 0;
+                if (!headerDone && replyLen < reply.length) {
+                    reply[replyLen++] = (byte) i;
+                }
+            }
+        }
+
+        String replyStr;
+        try {
+            replyStr = new String(reply, 0, replyLen, "ASCII7");
+        } catch (UnsupportedEncodingException ignored) {
+            replyStr = new String(reply, 0, replyLen);
+        }
+
+        // check if connect request was successfully
+        if (!(replyStr.startsWith("HTTP/1.0 200") || replyStr.startsWith("HTTP/1.1 200"))) {
+            throw new IOException("Unable to tunnel through " + tunnel_host + ":" + tunnel_port + ".  Proxy returns \"" + replyStr + "\"");
+        }
+
+        // success
     }
 }
